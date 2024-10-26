ba8806e3ac19db7dfcd46bfb77a21bb66bbe3114
tracy <keeyatracy@gmail.com>
2024-10-26 18:25:25 +0300

    Add 40% tax calculation for income over 36000

--- tax_calculator.py
+++ tax_calculator.py
@@ -7,3 +7,11 @@ def calculate_tax(earnings):
         return 0
     elif 12000 <= earnings < 36000:
         return (earnings - 12000) * 0.20
+
+def calculate_tax(earnings):
+    if earnings < 12000:
+        return 0
+    elif 12000 <= earnings < 36000:
+        return (earnings - 12000) * 0.20
+    else:
+        return (36000 - 12000) * 0.20 + (earnings - 36000) * 0.40
--- tests/test_tax_calculator.py
+++ tests/test_tax_calculator.py
@@ -7,3 +7,6 @@ class TestTaxCalculator(unittest.TestCase):
 
     def test_low_income_tax(self):
         self.assertEqual(calculate_tax(15000), 600)  # 20% of (15000 - 12000)
+
+    def test_high_income_tax(self):
+        self.assertEqual(calculate_tax(40000), 6400)  # 20% of (36000 - 12000) + 40% of (40000 - 36000)
