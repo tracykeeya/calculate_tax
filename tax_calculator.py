38978951a19360d1b79e2d3093240b95bb52f3a3
tracy <keeyatracy@gmail.com>
2024-10-26 18:19:58 +0300

    Add 20% tax calculation for income between 12000 and 36000

--- tax_calculator.py
+++ tax_calculator.py
@@ -1,3 +1,9 @@
 def calculate_tax(earnings):
     if earnings < 12000:
         return 0
+
+def calculate_tax(earnings):
+    if earnings < 12000:
+        return 0
+    elif 12000 <= earnings < 36000:
+        return (earnings - 12000) * 0.20
--- tests/test_tax_calculator.py
+++ tests/test_tax_calculator.py
@@ -5,5 +5,5 @@ class TestTaxCalculator(unittest.TestCase):
     def test_no_tax(self):
         self.assertEqual(calculate_tax(10000), 0)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_low_income_tax(self):
+        self.assertEqual(calculate_tax(15000), 600)  # 20% of (15000 - 12000)
